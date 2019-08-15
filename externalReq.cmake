596326199a0c632d9dbacb0e2661b070aa94693e
Sailanarmo <rlramirez12@gmail.com>
2019-08-15 12:16:51 -0600

    Testing yasm on windows.

--- externalReq.cmake
+++ externalReq.cmake
@@ -1,8 +1,16 @@
 find_program(YASM_EXE NAMES yasm nasm)
 
 if(NOT YASM_EXE)
-  message(FATAL_ERROR "
-  yasm/nasm has not been detected! It is required for FFmpeg Libraries 
-  Progam on Ubuntu can be installed with: sudo apt install yasm
-  ")
+  if( UNIX )
+    message(FATAL_ERROR "
+    yasm/nasm has not been detected! It is required for FFmpeg Libraries! 
+    Progam on Ubuntu can be installed with: sudo apt install yasm
+    ")
+  elseif( WIN32 )
+    message(FATAL_ERROR "
+    yasm/nasm has not been detected! It is required for FFmpeg Libraries! 
+    Progam on Windows can be installed with the provided executable in provided
+    Required Programs Folder.
+    ")
+  endif()
 endif()
