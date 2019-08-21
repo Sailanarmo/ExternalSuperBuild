66aa206b3204d0eeeab19e893b10a48a09cd277f
Sailanarmo <rlramirez12@gmail.com>
2019-08-21 10:55:45 -0600

    Testing FFmpeg with Windows.

--- Superbuild/FFmpegExternal_Win_Apple.cmake
+++ Superbuild/FFmpegExternal_Win_Apple.cmake
@@ -7,14 +7,6 @@ elseif( APPLE )
   set( FFmpeg_url "https://ffmpeg.zeranoe.com/builds/macos64/dev/ffmpeg-4.2-macos64-dev.zip")
 endif()
 
-find_program(YASM_EXE NAMES yasm nasm)
-
-if(NOT YASM_EXE)
-  set(FFmpeg_depends "yasm_external_download")
-else()
-  set(FFmpeg_depends "")
-endif()
-
 ExternalProject_Add(FFmpeg_external_download
   URL ${FFmpeg_url}
   UPDATE_COMMAND ""
@@ -28,12 +20,36 @@ ExternalProject_Add(FFmpeg_external_download
 #CACHE PATH "" seems to write the path to a file that I can set 
 #library paths to. 
 
-ExternalProject_Get_Property(FFmpeg_external_download BINARY_DIR)
 ExternalProject_Get_Property(FFmpeg_external_download SOURCE_DIR)
 
 set(FFmpeg_LIBRARY_DIR ${SOURCE_DIR}/lib CACHE INTERNAL "")
 set(FFmpeg_INCLUDE_DIR ${SOURCE_DIR}/include CACHE INTERNAL "")
 
+if(WIN32)
+  if(MSVC)
+    set(prefix "")
+    set(suffix ".lib")
+  endif()
+  if(MINGW)
+    set(prefix "lib")
+    set(suffix ".dll.a")
+  endif()
+elseif(APPLE)
+  set(prefix "lib")
+  set(suffix ".a")
+endif()
+
 add_library(FFmpeg_external STATIC IMPORTED)
 
+set(FFmpeg_LIBRARIES
+  ${FFmpeg_LIBRARY_DIR}/${prefix}avutil${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}avformat${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}avcodec${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}avdevice${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}avfilter${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}postproc${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}swresample${suffix}
+  ${FFmpeg_LIBRARY_DIR}/${prefix}swscale${suffix}
+  CACHE INTERNAL "" 
+)
 message(STATUS "FFmpeg_DIR: ${FFmpeg_LIBRARY_DIR}")
